adc0769a1a07be603bcef863e343fb4d8c3cf610
Savchenko <savchenko.ra@yandex.ru>
2023-02-27 13:21:22 +0300

    Task 2 + correct task 1.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
     public static void task1() {
         System.out.println("Task 1.");
@@ -9,5 +10,28 @@ public class Main {
                 nums[1] = 2;
                 nums[2] = 3;
         }
+        double[] numsDouble = {1.57, 7.654, 9.986};
+        String[] months = {
+                "Январь",
+                "Февраль",
+                "Март"
+        };
+    }
+    public static void task2() {
+        System.out.println("Task 2.");
+        int[] nums = new int[3]; {
+            nums[0] = 1;
+            nums[1] = 2;
+            nums[2] = 3;
+        }
+        double[] numsDouble = {1.57, 7.654, 9.986};
+        String[] months = {
+                "Январь",
+                "Февраль",
+                "Март"
+        };
+        System.out.println(nums[0] + ", " + nums[1] + ", " + nums[2]);
+        System.out.println(numsDouble[0] + ", " + numsDouble[1] + ", " + numsDouble[2]);
+        System.out.println(months[0] + ", " + months[1] + ", " + months[2]);
     }
 }

89057d95ee94174f9acb2e58c67f9f6ae9344611
Savchenko <savchenko.ra@yandex.ru>
2023-02-27 12:56:29 +0300

    Task 1.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+    public static void task1() {
+        System.out.println("Task 1.");
+        int[] nums = new int[3]; {
+                nums[0] = 1;
+                nums[1] = 2;
+                nums[2] = 3;
+        }
     }
 }

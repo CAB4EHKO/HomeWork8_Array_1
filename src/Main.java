715fe1389cd84f7b0669bdcb4e2ef74a3a9bc00f
Savchenko <savchenko.ra@yandex.ru>
2023-02-28 11:34:07 +0300

    Task №3 + correct task №2.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         task1();
@@ -8,11 +10,6 @@ public class Main {
     public static void task1() {
         System.out.println("Task 1.");
         int[] nums = new int[3];
-        {
-            nums[0] = 1;
-            nums[1] = 2;
-            nums[2] = 3;
-        }
         double[] numsDouble = {1.57, 7.654, 9.986};
         String[] months = {
                 "Январь",
@@ -24,38 +21,68 @@ public class Main {
     public static void task2() {
         System.out.println("Task 2.");
         int[] nums = new int[3];
-        {
-            nums[0] = 1;
-            nums[1] = 2;
-            nums[2] = 3;
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = i + 1;
+            if (i == nums.length - 1) {
+                System.out.println(nums[i]);
+                break;
+            }
+            System.out.print(nums[i] + ", ");
         }
         double[] numsDouble = {1.57, 7.654, 9.986};
+        for (int i = 0; i < numsDouble.length; i++) {
+            if (i == numsDouble.length - 1) {
+                System.out.println(numsDouble[i]);
+                break;
+            }
+            System.out.print(numsDouble[i] + ", ");
+        }
         String[] months = {
                 "Январь",
                 "Февраль",
                 "Март"
         };
-        System.out.println(nums[0] + ", " + nums[1] + ", " + nums[2]);
-        System.out.println(numsDouble[0] + ", " + numsDouble[1] + ", " + numsDouble[2]);
-        System.out.println(months[0] + ", " + months[1] + ", " + months[2]);
+        for (int i = 0; i < months.length; i++) {
+            if (i == months.length - 1) {
+                System.out.println(months[i]);
+                break;
+            }
+            System.out.print(months[i] + ", ");
+        }
     }
 
     public static void task3() {
         System.out.println("Task 3.");
         int[] nums = new int[3];
-        {
-            nums[0] = 1;
-            nums[1] = 2;
-            nums[2] = 3;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            nums[i] = i + 1;
+            if (i == 0) {
+                System.out.println(nums[i]);
+                break;
+            }
+            System.out.print(nums[i] + ", ");
         }
         double[] numsDouble = {1.57, 7.654, 9.986};
+        for (int i = numsDouble.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.println(numsDouble[i]);
+                break;
+            }
+            System.out.print(numsDouble[i] + ", ");
+        }
+
         String[] months = {
                 "Январь",
                 "Февраль",
                 "Март"
         };
-        System.out.println(nums[2] + ", " + nums[1] + ", " + nums[0]);
-        System.out.println(numsDouble[2] + ", " + numsDouble[1] + ", " + numsDouble[0]);
-        System.out.println(months[2] + ", " + months[1] + ", " + months[0]);
+        for (int i = months.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.println(months[i]);
+                break;
+            }
+            System.out.print(months[i] + ", ");
+        }
     }
 }
